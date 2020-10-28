013b4a9e7cc9463b32451ae6c47bfa3bc88247bd
priyanka9179500311 <py63158@gmail.com>
2020-10-28 19:01:43 +0530

    Myclass.php2

---
+++ container.php
@@ -0,0 +1,41 @@
+!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Bootstrap 4 Example</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<style>
+			
+			.a_row{
+				height:200px;
+				border:1px dashed red;
+			}
+			.green{
+				background-color:green;
+			}
+			.purple{
+				background-color:purple;
+			}
+			.blue{
+				background-color:blue;
+			}
+			
+		</style>
+
+	</head>
+	<body>
+		<div class ="row a_row">
+			<div class="col-sm-8 col-md-4 col-lg-6 col-xl-1  green">A</div>
+			<div class="col-sm-2 col-md-4 col-lg-3 col-xl-10  purple">B</div>
+			<div class="col-sm-2 col-md-4 col-lg-3 col-xl-1 blue">C</div>
+		
+		</div>
+
+		
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	</body>
+</html>
